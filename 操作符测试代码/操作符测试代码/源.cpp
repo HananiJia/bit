61f1960e38b20822eed0e0a5d2260f5f4bae0285
HananiJia <31822623+HananiJia@users.noreply.github.com>
2017-12-09 00:07:20 +0800

    Add files via upload

---
+++ 操作符测试代码/操作符测试代码/源.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+
+	int a = 12;
+	int b = 3;
+	a = a >>1;
+	printf("%d", a);
+
+	/*if ((a == 1) && (b == 1))
+		printf("a、b都为1");
+	else
+		printf("a、b不都为1");*/
+
+
+	/*int a = b = c = 1;
+	int a, b, c;
+	a = b = c = 1;*/
+
+	//int a = 1;
+	//int b = 0;
+	//b = ++a;
+	//printf("a=%d,b=%d", a, b);
+	/*int  i = 3;
+	double m = 2.7;
+	double n = 0;;
+	n = i%m;*/
+	/*n = m + i;
+	printf("%lf", n);
+	getchar();*/
+	system("pause");
+}
