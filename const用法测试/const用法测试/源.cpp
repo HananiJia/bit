5aeb5d3be214936baa8b5c2da9574ab6c50613a0
HananiJia <31822623+HananiJia@users.noreply.github.com>
2018-06-03 17:22:50 +0800

    Add files via upload

---
+++ const用法测试/const用法测试/源.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//int a = 10;
+//const int b = a;
+//
+//const int a = 10;
+//int b = a;
+//int a = 10;
+//const int *p1 = &a;
+//int *p2 = p1;
+
+//int const *p1 = &a;
+//int *p2 = p1;
+
+//int *const p1 = &a;
+//int *p2 = p1;
+//const int a = 10;
+//int &b = a;
+const int *p;
+int const *p;
+int * const p;
