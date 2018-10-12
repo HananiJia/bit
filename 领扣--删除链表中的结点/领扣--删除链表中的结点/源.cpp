6bf4fe8f56eb610d22b2e7d78b3f069daecbdb59
HananiJia <31822623+HananiJia@users.noreply.github.com>
2018-10-11 18:49:52 -0700

    Add files via upload

---
+++ 领扣--删除链表中的结点/领扣--删除链表中的结点/源.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+
+
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+
+class Solution {
+public:
+	void deleteNode(ListNode* node) {
+		node->val = node->next->val;
+		node->next = node->next->next;
+	}
+};
