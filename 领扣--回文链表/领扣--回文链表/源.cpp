4f892735263a2f0c1b793dc5ee7a8fb7d251ed21
HananiJia <31822623+HananiJia@users.noreply.github.com>
2018-11-12 19:36:50 -0800

    Add files via upload

---
+++ 领扣--回文链表/领扣--回文链表/源.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+
+class Solution {
+public:
+	bool isPalindrome(ListNode* head) {
+
+	}
+};
